8ca6b8a5d374d2a910702ec61f4b98e77ceaa3a5
Andrei Neculau <andrei.neculau@gmail.com>
2015-07-26 21:18:38 +0200

    tastebin_list and tastebin_get helpers for the curl/httpie

--- tastebin.curl.sh
+++ tastebin.curl.sh
@@ -1,5 +1,15 @@
 # export TASTEBIN_URL=
 
+tastebin_list() {
+    local URL=${TASTEBIN_URL}/tastes/
+    curl -sS -X GET ${URL} | sed "s|\([^ ]\+\) \([^ ]\+\) |\1 \2 ${URL}|"
+}
+
+tastebin_get() {
+    local URL=${TASTEBIN_URL}/tastes/${1}
+    curl -sS -X GET ${URL}
+}
+
 tastebin_save() {
     local URL=${TASTEBIN_URL}/tastes/
     curl -sS -i -X POST ${URL} | sed "s|^Location: |Location: ${URL}|"
--- tastebin.httpie.sh
+++ tastebin.httpie.sh
@@ -1,5 +1,15 @@
 # export TASTEBIN_URL=
 
+tastebin_list() {
+    local URL=${TASTEBIN_URL}/tastes/
+    http --check-status --pretty=all GET ${URL} | sed "s|\([^ ]\+\) \([^ ]\+\) |\1 \2 ${URL}|"
+}
+
+tastebin_get() {
+    local URL=${TASTEBIN_URL}/tastes/${1}
+    http --check-status --pretty=all GET ${URL}
+}
+
 tastebin_save() {
     local URL=${TASTEBIN_URL}/tastes/
     http --check-status -p hb --pretty=all POST ${URL} | sed "s|Location\(.*\):\(.*\) |Location\1:\2 ${URL}|"
